d2b4b56ae15254499b841bd3d366b5ad61182b39
badcyc <13949569027@163.com>
2018-03-08 20:50:26 +0800

    add convert

---
+++ Convert.java
@@ -0,0 +1,34 @@
+package dataStructure;
+
+
+
+public class Convert {
+    public static String convertString(String s,int nRows){
+        char[] c = s.toCharArray();
+        int len = c.length;
+        StringBuffer[] sb = new StringBuffer[nRows];
+        for (int i = 0; i < sb.length; i++) sb[i] = new StringBuffer();
+
+        int i = 0;
+        while (i < len) {
+            for (int idx = 0; idx < nRows && i < len; idx++) // vertically down
+                sb[idx].append(c[i++]);
+            for (int idx = nRows-2; idx >= 1 && i < len; idx--) // obliquely up
+                sb[idx].append(c[i++]);
+        }
+        for (int idx = 1; idx < sb.length; idx++)
+            sb[0].append(sb[idx]);
+        return sb[0].toString();
+    }
+
+    /*
+
+    test
+     */
+    public static void main(String[] args){
+        String s="PAYPALISHIRING";
+        String result;
+        result=convertString(s,3);
+        System.out.print(result);
+    }
+}
