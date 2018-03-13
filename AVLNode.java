88aaa4c4b38b1b039745e173d6abd3d7e0ab44d3
badcyc <badcyc@bingyan.net>
2018-03-13 20:50:55 +0800

    add AVLTree

---
+++ AVLNode.java
@@ -0,0 +1,22 @@
+package AVLtree;
+
+public class AVLNode<T extends Comparable> {
+    public AVLNode<T> left;
+    public AVLNode<T> right;
+    public T data;
+    public int height;
+    public AVLNode(T data){
+        this(data,null,null);
+    }
+    public AVLNode(T data,AVLNode<T> left,AVLNode<T> right){
+        this(data,left,right,0);
+    }
+    public AVLNode(T data,AVLNode<T> left,AVLNode<T> right,int height){
+        this.left=left;
+        this.right=right;
+        this.data=data;
+        this.height=height;
+    }
+
+
+}
---
+++ ITree.java
@@ -0,0 +1,16 @@
+package AVLtree;
+
+public interface ITree<T extends Comparable> {
+    boolean isEmpty();
+    int size();
+    int height();
+    String preOrder();
+    String inOrder();
+    String levelOeder();
+    void insert(T data);
+    void remove(T data);
+    T findMin();
+    T findMax();
+    boolean contains(T data)throws Exception;
+    void clear();
+}
