1918b05d347265f64824dd92d38c16321e5acc8b
badcyc <13949569027@163.com>
2018-10-30 00:55:37 +0800

    BinaryInsert

--- DynamicProgram/LongestValidParentheses.java
+++ DynamicProgram/LongestValidParentheses.java
@@ -16,11 +16,6 @@ import java.util.Stack;
  */
 public class LongestValidParentheses {
     public static boolean isValid(String s) {
-//        String reverse=new StringBuilder(s).reverse().toString();
-//        if (s.equals(reverse)){
-//            return true;
-//        }
-//        return false;
         Stack<Character> characters = new Stack<>();
         for (int i = 0; i < s.length(); i++) {
             if (s.charAt(i) == '(') {
@@ -29,11 +24,11 @@ public class LongestValidParentheses {
                 characters.pop();
             } else return false;
         }
-        return true;
+        return characters.empty();
     }
 
     public static int longestValidParentheses(String s) {
-        int max = -1;
+        int max = 0;
         for (int i = 0; i < s.length(); i++) {
             for (int j = i + 2; j <= s.length(); j += 2) {
                 if (isValid(s.substring(i, j))) {
@@ -44,8 +39,32 @@ public class LongestValidParentheses {
         return max;
     }
 
+    /**
+     * @Description: 动态规划解决 牛逼的解法
+     * @Param: [args]
+     * @return: void
+     * @Author: Yichao Chen
+     * @Date: 18-10-30
+     * @Time: 上午12:22
+     **/
+    public static int DP(String s) {
+        int maxans = 0;
+        int dp[] = new int[s.length()];
+        for (int i = 1; i < s.length(); i++) {
+            if (s.charAt(i) == ')') {
+                if (s.charAt(i - 1) == '(') {
+                    dp[i] = (i >= 2 ? dp[i - 2] : 0) + 2;
+                } else if (i - dp[i - 1] > 0 && s.charAt(i - dp[i - 1] - 1) == '(') {
+                    dp[i] = dp[i - 1] + ((i - dp[i - 1]) >= 2 ? dp[i - dp[i - 1] - 2] : 0) + 2;
+                }
+                maxans = Math.max(maxans, dp[i]);
+            }
+        }
+        return maxans;
+    }
+
     public static void main(String[] args) {
-        String s = "))((())";
+        String s = "()(()";
         int max = longestValidParentheses(s);
         System.out.println(max);
     }
